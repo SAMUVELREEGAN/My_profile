852f2bf46e60f763b233fed1a590f05f1782849e
SAMUVEL REEGAN P <164333238+SAMUVELREEGAN@users.noreply.github.com>
2025-08-25 10:59:27 +0530

    Update workExperiencePage.js

--- src/FreelanceAndDeveloper/json/workExperiencePage.js
+++ src/FreelanceAndDeveloper/json/workExperiencePage.js
@@ -1,4 +1,5 @@
-import logo from '../assets/company-logo.png';
+import logo1 from '../assets/comp.jfif';
+import logo2 from '../assets/comp.png';
 
 const workExperiencePage = [
   {
@@ -6,7 +7,7 @@ const workExperiencePage = [
     year: "2024 (6 Months)",
     company: "Edex Tech",
     location: "Madurai, India",
-    logo: logo,
+    logo: logo1,
     role: "Full Stack Developer (Intern)",
     description: "Worked on real-world full-stack applications using React.js, Node.js, and Django REST Framework. Developed REST APIs, implemented secure login with JWT, and deployed applications on AWS.",
     ctc: "Internship"
@@ -16,7 +17,7 @@ const workExperiencePage = [
     year: "2024 - 2025 (6 Months)",
     company: "TNQ Technologies",
     location: "Chennai, India",
-    logo: logo,
+    logo: logo2,
     role: "XML Error Processing Executive",
     description: "Handled and resolved XML file errors in digital publishing. Ensured accurate tagging and improved data quality in automated processing workflows.",
     ctc: "Fresher Role"
